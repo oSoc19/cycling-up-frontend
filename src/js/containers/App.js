7a79ad23f82bbe744e9dd1547541ad8bffd6bbbd
chloedevriese <chloe.devriese@student.howest.be>
2019-07-03 11:19:16 +0200

    Added some map structure

---
+++ src/js/containers/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../css/App.css';
+
+// import components
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         map
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
