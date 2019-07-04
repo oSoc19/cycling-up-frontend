db4b20ec39cb014ab3b17794816f5b313cc149d8
Daniëlle Terras <danielle.terras@student.howest.be>
2019-07-04 10:19:42 +0200

    Update splitscreen for the map with chart

--- src/js/components/Map/chart.jsx
+++ src/js/components/Map/chart.jsx
@@ -4,36 +4,20 @@ import { Polar } from "react-chartjs-2";
 import css from "./../../../css/chart.module.css";
 
 const data = {
-    datasets: [{
-      data: [
-        11,
-        16,
-        7,
-        3,
-        14
-      ],
-      backgroundColor: [
-        '#FF6384',
-        '#4BC0C0',
-        '#FFCE56',
-        '#E7E9ED',
-        '#36A2EB'
-      ],
-      label: 'My dataset' // for legend
-    }],
-    labels: [
-      'Red',
-      'Green',
-      'Yellow',
-      'Grey',
-      'Blue'
-    ]
+  datasets: [
+    {
+      data: [11, 16, 7, 3, 14],
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB"],
+      label: "My dataset" // for legend
+    }
+  ],
+  labels: ["Red", "Green", "Yellow", "Grey", "Blue"]
 };
 
 class Chart extends Component {
   render() {
     return (
-      <div className={css.splitScreen}>
+      <div className={css.halfScreen}>
         <div className={css.image} />
         <article
           style={{
@@ -43,12 +27,9 @@ class Chart extends Component {
             flexDirection: "column",
             marginLeft: "10rem",
             marginRight: "10rem"
-          }}>
-          <h2 className={css.h2}>
-            Would you consider commuting to work by bike?
-          </h2>
-        </article>
-        <article className={css.stats}>
+          }}
+        />
+        <article>
           <div className={css.intro}>
             <h2>Evolution of bike usage.</h2>
             <p className={css.subtitle}>TO COMMUTE TO WORK</p>
