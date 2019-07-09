a25637cf52f79590daa69942b92a755b7131a852
Daniëlle Terras <danielle.terras@student.howest.be>
2019-07-09 13:41:59 +0200

    Update historical data with graph and lorem ipsum

---
+++ src/js/index-script.js
@@ -0,0 +1,23 @@
+{
+  // Our labels along the x-axis
+  const years = [2005, 2011, 2014, 2019];
+  // For drawing the lines
+  const africa = [1.2, 1.9, 3, 3.1];
+
+  const ctx = document.getElementById(`canvas`);
+  const commuteChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: years,
+      datasets: [
+        {
+          data: africa,
+          label: '% of cyclist that commute to work',
+          borderColor: 'rgba(185, 206, 228, .1)',
+          fill: '#f9b138',
+          backgroundColor: '#f9b138'
+        }
+      ]
+    }
+  });
+}
