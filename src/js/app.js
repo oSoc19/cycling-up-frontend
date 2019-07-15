c4f55125ced4ce0f0a3401fbc737a48056222472
chloedevriese <chloe.devriese@student.howest.be>
2019-07-15 22:17:27 +0200

    Added javascript for navigation mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,37 @@
 // import evolution from './module/km-evolution';
-// import commute from './module/commute-to-work';
+//import commute from './module/commute-to-work';
 
-const init = () => {
-  // evolution();
-  // commute();
 
+let $navDestinationTargets, $mobileMenu, main;
+
+const getDomElements = () => {
+  $navDestinationTargets = document.querySelectorAll('.js-toggle-menu');
+  main = document.querySelector('main');
+  $mobileMenu = document.querySelector('.l-header__toggle');
+  $mobileMenu.addEventListener('click', onHandlerMenuClick);
 };
 
-init();
+const init = function () {
+  getDomElements();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.info('DOM loaded');
+  init();
+});
+
+
+const onHandlerMenuClick = () => {
+  $navDestinationTargets.forEach(element => {
+    console.log(element);
+
+    element.classList.toggle('is-active');
+
+    if (element.classList.contains('is-active')) {
+      main.classList.add('disable-scroll');
+    } else {
+      main.classList.remove('disable-scroll');
+    }
+
+  });
+}
