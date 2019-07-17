997e06708ee6027d501d12ea502f9a4f4f295ed6
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-17 11:17:33 +0200

    Complete the historical map

--- src/js/module/historical-map.js
+++ src/js/module/historical-map.js
@@ -1,14 +1,43 @@
 import mapboxgl from 'mapbox-gl';
 
-export default map => {
+const HISTO_MAP_URL = process.env.API_URL + '/map/historical/';
+
+console.log(HISTO_MAP_URL + "2019")
+
+
+export default $mapContainer => {
   mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
 
   const historicalMap = new mapboxgl.Map({
-    container: map,
+    container: $mapContainer,
     style: 'mapbox://styles/danielleterras/cjxoemrlt08nn1cmlm2owey24',
     zoom: 13.5,
     center: [4.35500, 50.84700]
   });
+
+  historicalMap.on('load', () => {
+
+    historicalMap.addSource('api_cycling_historical_map', {
+      type: 'geojson',
+      data: HISTO_MAP_URL + '2019'
+    })
+
+    historicalMap.addLayer({
+      id: 'historical_map',
+      type: 'line',
+      source: 'api_cycling_historical_map',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#EAB818',
+        'line-width': 3
+      }
+    });
+  });
+
+
 
   window.addEventListener('load', () => {
     historicalMap.resize();
