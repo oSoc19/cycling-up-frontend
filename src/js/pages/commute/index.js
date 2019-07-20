b07605e288734f9aaf572f9326e58fe88d6d88e3
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-20 13:23:32 +0200

    Init the pages folder

---
+++ src/js/pages/commute/index.js
@@ -0,0 +1,38 @@
+// -------------------------------------------------------------------
+// Dependencies
+
+// Import
+
+
+// Built-in
+
+
+// Mine
+
+
+// -------------------------------------------------------------------
+// Properties
+
+
+
+
+// -------------------------------------------------------------------
+// Exports
+
+
+/**
+ * Load the page with the necessary content (map, chart, ...)
+ */
+export function init() {
+
+}
+
+
+/**
+ *
+ * @param {string} lang  - The language selected
+ * @param {Object} translations - The translations data for the current page
+ */
+export function changeLanguage(lang, translations){
+
+}
