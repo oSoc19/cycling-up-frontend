c69ac789ec69e103d6b9d3d75c68a5921cbcd6f9
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-23 10:32:57 +0200

    Display empty chart on startup

--- src/js/pages/bike-count/bike-count-season-chart.js
+++ src/js/pages/bike-count/bike-count-season-chart.js
@@ -29,7 +29,7 @@ const API_BIKE_COUNT_BY_STATION_ID = process.env.API_URL + "/chart/bike_count/";
 
 // For drawing the lines
 
-const count = [
+const counts = [
   {
     id: 'jan',
     label: 'January',
@@ -40,19 +40,19 @@ const count = [
     id: 'may',
     label: 'May',
     borderColor: 'rgba(239, 185, 52, 1)',
-    backgroundColor: 'rgba(239, 185, 52, 1)',
+    backgroundColor: 'rgba(239, 185, 52, 1)'
   },
   {
     id: 'sep',
     label: 'September',
     borderColor: 'rgba(195, 214, 230, 1)',
-    backgroundColor: 'rgba(195, 214, 230, 1)',
+    backgroundColor: 'rgba(195, 214, 230, 1)'
   },
   {
     id: 'nov',
     label: 'November',
     borderColor: 'rgba(134, 147, 199, 1)',
-    backgroundColor: 'rgba(134, 147, 199, 1)',
+    backgroundColor: 'rgba(134, 147, 199, 1)'
   }
 ]
 
@@ -65,123 +65,16 @@ export function showChart(ctx) {
       type: 'line',
       data: {
         labels: years,
-        datasets: [
-          {
-            label: 'January',
-            borderColor: 'rgba(50, 54, 96, 1)',
-            backgroundColor: 'rgba(50, 54, 96, 1)',
-            data: [
-              277,
-              292,
-              302,
-              403,
-              395,
-              395,
-              395,
-              582,
-              661,
-              736,
-              962,
-              872,
-              936,
-              1544,
-              1438,
-              1432,
-              2323,
-              2304,
-              2408,
-              2810
-            ],
-            fill: false
-          },
-          {
-            label: 'May',
-            fill: false,
-            borderColor: 'rgba(239, 185, 52, 1)',
-            backgroundColor: 'rgba(239, 185, 52, 1)',
-            data: [
-              781,
-              598,
-              642,
-              836,
-              800,
-              964,
-              1253,
-              1416,
-              1370,
-              1724,
-              1829,
-              1961,
-              2433,
-              2669,
-              2491,
-              2398,
-              3267,
-              3222,
-              4609,
-              4432
-            ]
-          },
-          {
-            label: 'September',
-            fill: false,
-            borderColor: 'rgba(195, 214, 230, 1)',
-            backgroundColor: 'rgba(195, 214, 230, 1)',
-            data: [
-              635,
-              682,
-              747,
-              712,
-              904,
-              1254,
-              1221,
-              1651,
-              1672,
-              1644,
-              2177,
-              2397,
-              2446,
-              2615,
-              2792,
-              3250,
-              3622,
-              3538,
-              4801,
-              3966
-            ]
-          },
-          {
-            label: 'November',
-            fill: false,
-            borderColor: 'rgba(134, 147, 199, 1)',
-            backgroundColor: 'rgba(134, 147, 199, 1)',
-            data: [
-              337,
-              401,
-              490,
-              445,
-              508,
-              508,
-              508,
-              851,
-              994,
-              995,
-              1034,
-              1506,
-              1565,
-              2222,
-              2238,
-              2125,
-              2696,
-              3151,
-              3382,
-              3718
-            ]
-          }
-        ]
+        datasets: counts
       },
       options: {
-        responsive: false
+        responsive: false,
+        title: {
+          display: true,
+          fontSize: 16,
+          position : "bottom",
+          text: "Click on the blue circle on the map to get more info"
+        }
       }
     });
   }
@@ -203,7 +96,7 @@ export function onSelectedBikeStation(stationId, stationName) {
     .then(res => res.json())
     .then(({ properties }) => {
       const newDataSet = [];
-      for (const count_month of count) {
+      for (const count_month of counts) {
         const data = Object.values(properties.count_data[count_month.id])
         const dataset = Object.assign(count_month, {
           fill: false,
@@ -215,6 +108,8 @@ export function onSelectedBikeStation(stationId, stationName) {
       bikeCountChart.data.datasets = newDataSet;
       bikeCountChart.options.title = {
         display: true,
+        position : "bottom",
+        fontSize: 16,
         text: stationName
       };
       bikeCountChart.update()
