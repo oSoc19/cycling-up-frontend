d4b80c239d50986e8c11eb0e1d63bdda4ff5ef32
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-24 22:46:42 +0200

    Fetch && display cummulated kms

--- src/js/pages/evolution/km-evolution-chart.js
+++ src/js/pages/evolution/km-evolution-chart.js
@@ -1,37 +1,62 @@
 import Chart from 'chart.js';
 
-const apiChartCommuteDataUrl = `${process.env.API_URL}/chart/cumulated_kilometers`;
+const regionalKmChartDataUrl = `${process.env.API_URL}/chart/cumulated_kilometers_regional_roads`;
+const gfrKmChartDataUrl = `${process.env.API_URL}/chart/cumulated_kilometers_gfr`;
 
 let evolutionChart;
 
 const fetchData = () => {
-  return fetch(apiChartCommuteDataUrl).then(r => r.json());
+  return Promise.all([
+    regionalKmChartDataUrl,
+    gfrKmChartDataUrl
+  ].map(url =>
+    fetch(url).then(res => res.json())
+  ))
+
+
 };
 
 export function init(ctx) {
   return fetchData().then(data => showChart(ctx, data));
 }
 
-export function showChart(ctx, chartData) {
+export function showChart(ctx, [regionalData, gfrData]) {
+
+  const datasets = [
+    {
+      id: 'region',
+      label: 'Cumulated amount of km per year of regionnal paths',
+      borderColor: 'rgba(239, 185, 52, 1)',
+      backgroundColor: 'rgba(239, 185, 52, 1)',
+      data : [],
+      fill: false,
+      spanGaps: true,
+    },
+    {
+      id: 'gfr',
+      label: 'Cumulated amount of km per year of GFR paths',
+      borderColor: 'rgba(195, 214, 230, 1)',
+      backgroundColor: 'rgba(195, 214, 230, 1)',
+      data : [],
+      fill: false,
+      spanGaps: true,
+    }
+  ]
   // Our labels along the x-axis
-  const years = chartData.map(d => d.year);
+  const years = regionalData.map(rd => rd.year);
+
   // For drawing the lines
-  const km = chartData.map(d => d.cumulated_kilometers);
+  const sortChartData = (d1, d2) => d1['year'] - d2['year'];
+  datasets[0].data = regionalData.map(d => ({x : d['year'], y : d['cumulated_kilometers'] })).sort(sortChartData);
+  datasets[1].data = gfrData.map(d => ({x : d['year'], y : d['cumulated_kilometers'] })).sort(sortChartData);
 
   if (ctx) {
     evolutionChart = new Chart(ctx, {
       type: 'line',
+      // labels,
       data: {
-        labels: years,
-        datasets: [
-          {
-            data: km,
-            label: 'Total amount of km per year of cyclable paths',
-            borderColor: '#f9b138',
-            fill: '#fff',
-            backgroundColor: '#EAB818'
-          }
-        ]
+        labels : years,
+        datasets
       },
       options: {
         responsive: false
